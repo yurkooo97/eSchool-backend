74a98575024db19cdb3d2ca71acbd5f3b08b5807
Yura Tykhan <jura1997tikhan@gmail.com>
2023-10-10 17:24:52 +0300

    changed dockerfile

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,12 +11,12 @@ source ./$BASEDIR/.env
 docker network create eschool-network
 
 # starting container with mysql DB
-docker run --name eschool-mysql \
-    -p :3306 \
-    --network eschool-network \
-    -e MYSQL_ROOT_PASSWORD=$DATASOURCE_PASSWORD \
-    -e MYSQL_DATABASE=$MYSQL_DATABASE \
-    -d mysql:5.6
+# docker run --name eschool-mysql \
+#     -p :3306 \
+#     --network eschool-network \
+#     -e MYSQL_ROOT_PASSWORD=$DATASOURCE_PASSWORD \
+#     -e MYSQL_DATABASE=$MYSQL_DATABASE \
+#     -d mysql:5.6
 
 # building app image
 docker build -t eschool:1.0 .
